05bcacbaef5e396c2c20c5ff4bbb68002d67d0fb
Egor Bichurin <bichurin.es@gmail.com>
2020-11-30 08:20:37 +0500

    fix: исправления по замечаниям ревью

--- script/script.js
+++ script/script.js
@@ -1,57 +1,38 @@
+// Объявляем глобальные переменные
+
+let popup = document.querySelector('.popup'),
+    name = document.querySelector('.profile__title'),
+    description = document.querySelector('.profile__subtitle'),
+    nameField = popup.querySelector('.popup__form-field_type_name'),
+    aboutField = popup.querySelector('.popup__form-field_type_about'),
+    editButton = document.querySelector('.profile__edit-button'),
+    closeButton = popup.querySelector('.popup__close-button'),
+    form = popup.querySelector('.popup__form');
+
 // Функционал кнопки редактирования профиля
 
-let editButton = document.querySelector('.profile__edit-button');
-
 function editProfile() {
-  let popup = document.querySelector('.popup'),
-      name = document.querySelector('.profile__title'),
-      description = document.querySelector('.profile__subtitle'),
-      nameField = popup.querySelector('.popup__form-field_type_name'),
-      aboutField = popup.querySelector('.popup__form-field_type_about');
-
   nameField.value = name.textContent;
   aboutField.value = description.textContent;
   popup.classList.add('popup_opened');
 }
 
-editButton.addEventListener('click', editProfile);
-
 // Функционал кнопки закрытия формы по кнопке или по щелчку вне зоны формы
 
 function closeForm() {
-  let popup = document.querySelector('.popup');
   popup.classList.remove('popup_opened');
 }
 
-function closeFormFromClick(event) {
-  let closeButton = document.querySelector('.popup__close-button'),
-      popup = document.querySelector('.popup');
-
-  if (event.target !== popup && event.target !== closeButton) {
-    return;
-  }
-
-  closeForm();
-}
-
-document.addEventListener('click', closeFormFromClick);
-
 // Функционал кнопки сохранения формы
-
-let form = document.querySelector('.popup__form');
 
 function changeInfo(event) {
   event.preventDefault();
-
-  let name = document.querySelector('.profile__title'),
-      description = document.querySelector('.profile__subtitle'),
-      nameField = document.querySelector('.popup__form-field_type_name'),
-      aboutField = document.querySelector('.popup__form-field_type_about');
-
   name.textContent = nameField.value;
   description.textContent = aboutField.value;
-
   closeForm();
 }
 
+// Привязываем функции к событиям
+editButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closeForm);
 form.addEventListener('submit', changeInfo);
