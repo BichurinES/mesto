962b4a0ba451bbed02d4be6216a7fb1aba17d666
Egor Bichurin <bichurin.es@gmail.com>
2021-02-13 18:23:45 +0500

    refactor: реализован класс Section

---
+++ script/Section.js
@@ -0,0 +1,17 @@
+export default class Section {
+  constructor({items, render}, containerSelector) {
+    this._items = items;
+    this._render = render;
+    this._container = document.querySelecor(containerSelector);
+  }
+
+  addItem(item) {
+    this._container.appendChild(item);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {
+      this._render(item);
+    });
+  }
+}
